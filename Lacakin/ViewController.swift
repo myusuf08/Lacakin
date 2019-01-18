899946a9eab7bceec6f99135be2a7a6d3b2f294c
yusufcircl <yusuf@circl.it>
2019-01-18 14:17:47 +0700

    Initial Commit

---
+++ Lacakin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lacakin
+//
+//  Created by Muhammad Yusuf on 18/01/19.
+//  Copyright © 2019 Gamatechno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
